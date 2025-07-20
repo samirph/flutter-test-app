e521699b4c5ed65465f1cd2eb80402330a849150
Samir <samirphaddad@gmail.com>
2025-07-20 20:16:49 -0300

    Move container logic to separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,32 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text('Playground de aplicativo do Samir!'),
-          ),
+        appBar: AppBar(
+          title: Text('Playground de aplicativo do Samir!'),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
+        body: HomeContainer(),
       ),
     );
+  }
+}
+
+
+class HomeContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 182, 225, 245), const Color.fromARGB(255, 3, 72, 129)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Text('Playground de aplicativo do Samir!', style: TextStyle(color: const Color.fromARGB(255, 1, 6, 65), fontSize: 20, fontWeight: FontWeight.bold),),
+          ),
+        );
   }
 }
